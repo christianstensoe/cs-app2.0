3125f71296e4ad2dfd6065ac80634be749e55570
Christian Stensøe <chstens@stud.ntnu.no>
2025-08-19 22:24:17 +0200

    fixfixfix

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -68,10 +68,7 @@ const Navbar = () => {
               <FaHome className="w-4 h-4" />
               <span>Home</span>
             </Link>
-            <Link href="/strava" className="nav-link">
-              <FaRunning className="w-4 h-4" />
-              <span>Strava</span>
-            </Link>
+
             <Link href="/resume" className="nav-link">
               <FaGraduationCap className="w-4 h-4" />
               <span>Resume</span>
